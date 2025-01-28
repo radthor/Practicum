a738eb6d5a48b2d553536c60b2b6a030628d7ce7
radthor <joslinaj@mail.uc.edu>
2025-01-27 21:44:18 -0500

    First commit :)

---
+++ src/PersonGenerator.java
@@ -0,0 +1,73 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PersonGenerator {
+    public static void main(String[] args) {
+        ArrayList<String> folks = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\personData.txt");
+
+        boolean done = false;
+        /* a. ID (a string)
+           b. FirstName
+           C.LastName
+           D. Title (a string like Mr., Mrs, Ms., Dr.)
+           e. YearOfBirth (an int)
+         */
+        String personRec = "";
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        int YOB = 0;
+
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter the ID [6 digits]: ");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter the first name: ");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter the last name: ");
+            title = SafeInput.getNonZeroLenString(in, "Enter the title: ");
+            YOB = SafeInput.getRangedInt(in, " Enter the year of birth: ", 1000, 9999);
+
+            personRec = ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB;
+            folks.add(personRec);
+
+            done= SafeInput.getYNConfirm(in, "Are you done?");
+        }while(!done);
+
+        for( String p: folks)
+            System.out.println(p);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file.toFile())))
+        {
+
+            // Finally can write the file LOL!
+
+            for(String rec : folks)
+            {
+                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
